1ec14c7824b7663d494e27fd9e95e74c1fb38837
Ritesh Ranjan <unsortedarray@Riteshs-MBP.Dlink>
2020-08-23 01:09:48 +0530

    another series

--- Yest.test.js
+++ Yest.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { mount, shallow } from 'enzyme';
 import App from '../src/App';
-import AddIssueButton from './components/AddIssueButton';
+import AddIssueButton from '../src/components/AddIssueButton';
 
 describe('App', () => {
   it('should render app without crashing', () => {
