a78d73bbe86f4b04953d4fc2aeecf358715bea19
Ritesh Ranjan <unsortedarray@Riteshs-MBP.Dlink>
2020-08-23 01:05:14 +0530

    another series

--- EnzymeCases.test.js
+++ EnzymeCases.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import IssuesList from '../src/components/IssuesList';
+import IssuesList from '../src/components/IssuesList/Issues.js';
 
 const issues = [
   {
