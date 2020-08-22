5417a3b3ec3030b5c75e8dc5046aedf47b56b512
Ritesh Ranjan <unsortedarray@Riteshs-MBP.Dlink>
2020-08-22 23:52:37 +0530

    test case FE

---
+++ Yest.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { mount, shallow } from 'enzyme';
+import App from './App';
+import AddIssueButton from './components/AddIssueButton';
+
+describe('App', () => {
+  it('should render app without crashing', () => {
+    mount(<App />);
+  });
+});
